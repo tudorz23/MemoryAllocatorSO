b43f2e367a5f664d15006b9759b1cfb2615aba5a
Tudor Zaharia <zahariatm@yahoo.com>
2023-11-06 16:45:04 +0200

    Coding style errors solved.

--- src/osmem.c
+++ src/osmem.c
@@ -48,11 +48,11 @@ void list_remove_block(block_meta_t *block)
 block_meta_t *map_block_in_mem(size_t size)
 {
 	size_t requested_size = (META_BLOCK_SIZE + size);
-	block_meta_t *block = mmap(NULL, requested_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	block_meta_t *block = mmap(NULL, requested_size, PROT_READ | PROT_WRITE,
+								MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 
-	if (block == MAP_FAILED) {
+	if (block == MAP_FAILED)
 		return NULL;
-	}
 
 	block->size = size;
 	block->status = STATUS_MAPPED;
@@ -68,21 +68,21 @@ block_meta_t *map_block_in_mem(size_t size)
  */
 int prealloc_heap_attempt(void)
 {
-	if (heap_prealloc_done != 0) {
+	if (heap_prealloc_done != 0)
 		return 1;
-	}
 
 	// Try to do the Heap Preallocation
 	void *request_block = sbrk(HEAP_PREALLOC_SIZE);
 
 	// Check if sbrk failed.
-	if (request_block == (void *) -1) {
+	if (request_block == (void *) -1)
 		return 0;
-	}
 
 	block_meta_t *prealloc_block = (block_meta_t *)request_block;
+
 	prealloc_block->size = HEAP_PREALLOC_SIZE - META_BLOCK_SIZE;
 	prealloc_block->status = STATUS_FREE;
+
 	list_add_last(prealloc_block);
 
 	heap_prealloc_done = 1;
@@ -102,9 +102,8 @@ block_meta_t *find_best_block(size_t size)
 
 	while (iterator != &head) {
 		if (iterator->status == STATUS_FREE && iterator->size >= ALIGN(size)) {
-			if (!best_fit || iterator->size < best_fit->size) {
+			if (!best_fit || iterator->size < best_fit->size)
 				best_fit = iterator;
-			}
 		}
 
 		iterator = iterator->next;
@@ -121,12 +120,11 @@ block_meta_t *find_best_block(size_t size)
  */
 void split_block_attempt(block_meta_t *block, size_t size)
 {
-	if (block->size == ALIGN(size)) {
+	if (block->size == ALIGN(size))
 		return;
-	}
 
-	// If split happens, the payload of @block would be occupied by the
-	// requested size and a new block_meta_t structure and at least 1 free byte.
+	// If split happens, payload of @block would be occupied by the requested
+	// size and a new block_meta_t structure and at least 1 free byte.
 	size_t minimum_occupied_size = ALIGN(size) + META_BLOCK_SIZE + 1;
 
 	if (minimum_occupied_size >= block->size) {
@@ -134,7 +132,8 @@ void split_block_attempt(block_meta_t *block, size_t size)
 		return;
 	}
 
-	block_meta_t *new_block = (block_meta_t *)((char *)block + META_BLOCK_SIZE + ALIGN(size));
+	block_meta_t *new_block = (block_meta_t *)((char *)block + META_BLOCK_SIZE
+								+ ALIGN(size));
 
 	new_block->size = block->size - ALIGN(size) - META_BLOCK_SIZE;
 	new_block->status = STATUS_FREE;
@@ -160,9 +159,8 @@ block_meta_t *expand_last_block(size_t size)
 	void *heap_end = (char *)last_block + META_BLOCK_SIZE + last_block->size;
 
 	heap_end = sbrk(additional_needed_size);
-	if (heap_end == (void *) -1) {
+	if (heap_end == (void *) -1)
 		return NULL;
-	}
 
 	last_block->size += additional_needed_size;
 	return last_block;
@@ -190,7 +188,8 @@ void coalesce_attempt(void)
 	block_meta_t *to_coalesce2 = NULL;
 
 	while (iterator != &head) {
-		if (iterator->status == STATUS_ALLOC || iterator->status == STATUS_MAPPED) {
+		if (iterator->status == STATUS_ALLOC
+			|| iterator->status == STATUS_MAPPED) {
 			to_coalesce1 = NULL;
 			to_coalesce2 = NULL;
 			iterator = iterator->next;
@@ -216,16 +215,15 @@ void coalesce_attempt(void)
 /**
  * Traverses the list, searching for a block whose payload's start
  * address is ptr.
- * @return the block, if existing, NULL, otherwise. 
+ * @return the block, if existing, NULL, otherwise.
  */
 block_meta_t *search_block_in_list(void *ptr)
 {
 	block_meta_t *iterator = head.next;
 
 	while (iterator != &head) {
-		if (((char *)iterator + META_BLOCK_SIZE) == ptr) {
+		if (((char *)iterator + META_BLOCK_SIZE) == ptr)
 			return iterator;
-		}
 
 		iterator = iterator->next;
 	}
@@ -262,9 +260,8 @@ block_meta_t *get_free_heap_block(size_t size)
 	if (head.prev->status == STATUS_FREE) {
 		block_meta_t *expanded_block = expand_last_block(ALIGN(size));
 
-		if (!expanded_block) {
+		if (!expanded_block)
 			return NULL;
-		}
 
 		return expanded_block;
 	}
@@ -272,29 +269,26 @@ block_meta_t *get_free_heap_block(size_t size)
 	// The last block is not free, so a new block is created.
 	void *request_block = sbrk(META_BLOCK_SIZE + ALIGN(size));
 
-	if (request_block == (void *) -1) {
+	if (request_block == (void *) -1)
 		return NULL;
-	}
 
 	block_meta_t *new_block = (block_meta_t *)request_block;
+
 	new_block->size = ALIGN(size);
 
 	list_add_last(new_block);
-	
+
 	return new_block;
 }
 
-
 void *os_malloc(size_t size)
 {
-	if (size <= 0) {
+	if (size <= 0)
 		return NULL;
-	}
 
 	// Check if the list head has been initialized
-	if (!head_init_done) {
+	if (!head_init_done)
 		head_init();
-	}
 
 	// The alignment is done before calling any function, so they
 	// ought not bother with alignment.
@@ -303,41 +297,39 @@ void *os_malloc(size_t size)
 	if (aligned_size + META_BLOCK_SIZE < MMAP_THRESHOLD) {
 		block_meta_t *heap_block = get_free_heap_block(aligned_size);
 
-		if (!heap_block) {
+		if (!heap_block)
 			return NULL;
-		}
 
 		heap_block->status = STATUS_ALLOC;
 		return (heap_block + 1);
 
 	} else {
 		block_meta_t *block = map_block_in_mem(aligned_size);
-		if (!block) {
+
+		if (!block)
 			return NULL;
-		}
+
 		return ((char *)block + META_BLOCK_SIZE);
 	}
 }
 
 void os_free(void *ptr)
 {
-	if (!ptr) {
+	if (!ptr)
 		return;
-	}
 
 	block_meta_t *block = search_block_in_list(ptr);
 
-	if (!block) {
+	if (!block)
 		return;
-	}
 
-	if (block->status == STATUS_FREE) {
+	if (block->status == STATUS_FREE)
 		return;
-	}
 
 	if (block->status == STATUS_MAPPED) {
 		list_remove_block(block);
 		size_t size_to_delete = META_BLOCK_SIZE + ALIGN(block->size);
+
 		munmap(block, size_to_delete);
 		return;
 	}
@@ -350,51 +342,51 @@ void os_free(void *ptr)
 
 void *os_calloc(size_t nmemb, size_t size)
 {
-	if (nmemb == 0 || size == 0) {
+	if (nmemb == 0 || size == 0)
 		return NULL;
-	}
 
-	if (!head_init_done) {
+	if (!head_init_done)
 		head_init();
-	}
 
 	size_t aligned_size = ALIGN(size * nmemb);
 
 	if ((long)(aligned_size + META_BLOCK_SIZE) < (long)getpagesize()) {
 		block_meta_t *heap_block = get_free_heap_block(aligned_size);
-		if (!heap_block) {
+
+		if (!heap_block)
 			return NULL;
-		}
 
 		heap_block->status = STATUS_ALLOC;
-		memset(heap_block + 1, 0, aligned_size);
-		return heap_block + 1;
-	} else {
-		block_meta_t *block = map_block_in_mem(aligned_size);
-		if (!block) {
-			return NULL;
-		}
-		void *result = (char *)block + META_BLOCK_SIZE;
-		memset(result, 0, aligned_size);
-		return result;
+		memset((char *)heap_block + META_BLOCK_SIZE, 0, aligned_size);
+		return ((char *)heap_block + META_BLOCK_SIZE);
 	}
+
+	block_meta_t *block = map_block_in_mem(aligned_size);
+
+	if (!block) 
+		return NULL;
+
+	void *result = (char *)block + META_BLOCK_SIZE;
+
+	memset(result, 0, aligned_size);
+	return result;
 }
 
-
-
-
-
+/**
+ * Remove a mapped block from the list and unmap its memory zone.
+ */
 void delete_mapped_block(block_meta_t *block)
 {
-	if (block->status != STATUS_MAPPED) {
+	if (block->status != STATUS_MAPPED)
 		return;
-	}
 
 	list_remove_block(block);
 	munmap(block, block->size + META_BLOCK_SIZE);
 }
 
-
+/**
+ * Copies size bytes from src's payload to dest's payload.
+ */
 void copy_block(block_meta_t *dest, block_meta_t *src, size_t size)
 {
 	void *dest_payload = (char *)dest + META_BLOCK_SIZE;
@@ -412,26 +404,27 @@ void *shrink_realloc(block_meta_t *block, size_t size)
 		if (size >= MMAP_THRESHOLD) {
 			// Shrink mapped block to another mapped block.
 			block_meta_t *new_map_block = map_block_in_mem(size);
-			if (!new_map_block) {
+
+			if (!new_map_block)
 				return NULL;
-			}
 
 			copy_block(new_map_block, block, new_map_block->size);
-			
+
 			delete_mapped_block(block);
 			return ((char *)new_map_block + META_BLOCK_SIZE);
 		}
 
 		// Shrink mapped block to a block on heap.
 		block_meta_t *heap_block = get_free_heap_block(size);
-		if (!heap_block) {
+
+		if (!heap_block)
 			return NULL;
-		}
-		
+
 		heap_block->status = STATUS_ALLOC;
-		
+
 		copy_block(heap_block, block, heap_block->size);
 		delete_mapped_block(block);
+
 		return ((char *)heap_block + META_BLOCK_SIZE);
 	}
 
@@ -440,6 +433,9 @@ void *shrink_realloc(block_meta_t *block, size_t size)
 	return ((char *)block + META_BLOCK_SIZE);
 }
 
+/**
+ * Coalesces block to adjacent free blocks until its size exceeds size.
+ */
 void block_coalesce_to_size(block_meta_t *block, size_t size)
 {
 	block_meta_t *iterator = block->next;
@@ -448,9 +444,8 @@ void block_coalesce_to_size(block_meta_t *block, size_t size)
 		if (iterator->status == STATUS_FREE) {
 			coalesce_blocks(block, iterator);
 
-			if (block->size >= size) {
+			if (block->size >= size)
 				break;
-			}
 
 			iterator = iterator->next;
 			continue;
@@ -467,24 +462,26 @@ void *extend_realloc(block_meta_t *block, size_t size)
 {
 	if (block->status == STATUS_MAPPED) {
 		block_meta_t *new_map_block = map_block_in_mem(size);
-		if (!new_map_block) {
+
+		if (!new_map_block)
 			return NULL;
-		}
 
 		copy_block(new_map_block, block, block->size);
 		delete_mapped_block(block);
+
 		return ((char *)new_map_block + META_BLOCK_SIZE);
 	}
 
 	// Original block was alloc'd.
 	if (size >= MMAP_THRESHOLD) {
 		block_meta_t *new_map_block = map_block_in_mem(size);
-		if (!new_map_block) {
+
+		if (!new_map_block)
 			return NULL;
-		}
 
 		copy_block(new_map_block, block, block->size);
 		block->status = STATUS_FREE;
+
 		return ((char *)new_map_block + META_BLOCK_SIZE);
 	}
 
@@ -493,16 +490,18 @@ void *extend_realloc(block_meta_t *block, size_t size)
 		size_t necessary_size = size - block->size;
 
 		void *added_size = sbrk(necessary_size);
-		if (!added_size) {
+
+		if (!added_size)
 			return NULL;
-		}
 
 		block->size = size;
+
 		return ((char *)block + META_BLOCK_SIZE);
 	}
 
 	// Try to extend current block, coalescing it to adjacent free blocks.
 	size_t original_block_size = block->size;
+
 	block_coalesce_to_size(block, size);
 
 	if (block->size >= size) {
@@ -512,9 +511,9 @@ void *extend_realloc(block_meta_t *block, size_t size)
 
 	// The block is still not big enough, so a reallocation is necessary.
 	block_meta_t *heap_block = get_free_heap_block(size);
-	if (!heap_block) {
+
+	if (!heap_block)
 		return NULL;
-	}
 
 	heap_block->status = STATUS_ALLOC;
 
@@ -524,12 +523,10 @@ void *extend_realloc(block_meta_t *block, size_t size)
 	return ((char *)heap_block + META_BLOCK_SIZE);
 }
 
-
 void *os_realloc(void *ptr, size_t size)
 {
-	if (ptr == NULL) {
+	if (ptr == NULL)
 		return os_malloc(size);
-	}
 
 	if (size == 0) {
 		os_free(ptr);
@@ -537,9 +534,9 @@ void *os_realloc(void *ptr, size_t size)
 	}
 
 	block_meta_t *req_block = search_block_in_list(ptr);
-	if (!req_block || req_block->status == STATUS_FREE) {
+
+	if (!req_block || req_block->status == STATUS_FREE)
 		return NULL;
-	}
 
 	size_t aligned_size = ALIGN(size);
 
@@ -548,12 +545,11 @@ void *os_realloc(void *ptr, size_t size)
 		return ((char *)req_block + META_BLOCK_SIZE);
 	}
 
-	if (aligned_size > req_block->size) {
+	if (aligned_size > req_block->size)
 		return extend_realloc(req_block, aligned_size);
-	}
 
-	if (aligned_size < req_block->size) {
+	if (aligned_size < req_block->size)
 		return shrink_realloc(req_block, aligned_size);
-	}
+
 	return NULL;
 }
--- utils/osmem.h
+++ utils/osmem.h
@@ -23,13 +23,19 @@ void *os_realloc(void *ptr, size_t size);
 void head_init(void);
 void list_add_last(block_meta_t *block);
 void list_remove_block(block_meta_t *block);
-block_meta_t *search_block_in_list(void *ptr);
 
 block_meta_t *map_block_in_mem(size_t size);
-
-int prealloc_heap_attempt();
+int prealloc_heap_attempt(void);
 block_meta_t *find_best_block(size_t size);
 void split_block_attempt(block_meta_t *block, size_t size);
 block_meta_t *expand_last_block(size_t size);
-
+void coalesce_blocks(block_meta_t *block1, block_meta_t *block2);
+void coalesce_attempt(void);
+block_meta_t *search_block_in_list(void *ptr);
 block_meta_t *get_free_heap_block(size_t size);
+
+void delete_mapped_block(block_meta_t *block);
+void copy_block(block_meta_t *dest, block_meta_t *src, size_t size);
+void *shrink_realloc(block_meta_t *block, size_t size);
+void block_coalesce_to_size(block_meta_t *block, size_t size);
+void *extend_realloc(block_meta_t *block, size_t size);
